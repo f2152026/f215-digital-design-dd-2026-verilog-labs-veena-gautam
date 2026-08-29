a5c593939b931eb242ae25567b8234a2853a3de2
VEENA GAUTAM <f20250133@pilani.bits-pilani.ac.in>
2026-08-29 15:19:51 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -22,23 +22,20 @@
 // Gate-level model of a 1-bit full adder with gate delays.
 
 module FA_Gate(
-
   input a,
   input b,
   input cin,
-
   output sum,
   output cout
-
 );
 
   wire ps, pc1, pc2;
 
-  xor #(2,3) (ps,  a,   b);
-  and #(2,3) (pc1, a,   b);
-  xor #(2,3) (sum, cin, ps);
-  and #(2,3) (pc2, cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
 
